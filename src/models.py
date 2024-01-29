57a21c57a1549fb17ba17ddd1ce1960106e889db
Intetsu0396 <141952373+Intetsu0396@users.noreply.github.com>
2024-01-29 16:47:38 +0000

    update

--- src/models.py
+++ src/models.py
@@ -1,32 +1,62 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, DateTime, Text
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = "user"
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+    email = Column(String(250), nullable = False)
+    favorites = relationship("favorites")
 
-    def to_dict(self):
-        return {}
+class People(Base):
+    __tablename__ = "people"
 
+    iud = Column(Integer, primary_key= True)
+    name = Column(String(250), nullable = False)
+    height = Column(Integer, nullable = False)
+    url = Column(String(400), nullable = False)
+    mass = Column(Integer, nullable = True)
+    hair_color = Column(String(100), nullable = True)
+    skin_color = Column(String(100), nullable = True)
+    eye_color = Column(String(100), nullable = True)
+    birth_year = Column(String(100), nullable = True)
+    gender = Column(String(100), nullable = True)
+    favorites = relationship("favorites")
+
+
+class Vehicle(Base):
+    __tablename__ = "vehicle"
+
+    iud = Column(Integer, primary_key = True)
+    name = Column(String(200), nullable = False)
+    model = Column(String(100), nullable = True)
+    url = Column(String(200), nullable = False)
+    vehicle_class = Column(String(50),nullable = True)
+    manufacturer = Column(String(50), nullable = True)
+    cost_in_credits = Column(Integer, nullable = True)
+    lenght = Column(Float, nullable = True)
+    crew = Column(Integer, nullable = True)
+    max_atmosphering_speed = Column(Integer, nullable = True)
+    cargo_capacity = Column(Integer, nullable = True)
+    consumable = Column(String(100), nullable= True)
+    favorites = relationship("favorites")
+
+class Favorites(Base): 
+    __tablename__ = "favorites"
+
+    fav_id = Column(Integer, primary_key = True)
+    People_uid = Column(Integer, ForeignKey("people.uid"))
+    vehicle_uid = Column(Integer, ForeignKey("vehicle.uid"))
+    planet_uid = Column(Integer, ForeignKey("planet.uid"))
+    user_uid = Column
+
+    
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
